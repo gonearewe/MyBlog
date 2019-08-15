3962b63210de744e8cb055a7214aae2afbb8324b
gonearewe <cristmactavish@outlook.com>
2019-08-15 15:53:21 +0800

    bascic function

---
+++ controllers/login.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"fmt"
+	"MyBlog/utils"
+	"MyBlog/models"
+	"github.com/astaxie/beego"
+)
+
+type LoginController struct {
+	beego.Controller
+}
+
+func (c *LoginController) Get() {
+	c.TplName = "login.html"
+}
+
+func (c *LoginController) Post() {
+	olduser:=new(models.User)
+	olduser.Username=c.GetString("username")
+	olduser.Password=utils.Md5(c.GetString("password")) 
+	if olduser.Exist()==false{
+		c.Data["json"]=map[string]interface{}{"code":4,"message":"用户名不存在"}
+		c.ServeJSON()
+		return
+	}
+
+	if actualuser,err:=olduser.GetByName();err!=nil{
+		fmt.Println(err)
+		c.Data["json"]=map[string]interface{}{"code":5,"message":"数据库查询出错"}
+		c.ServeJSON()
+		return
+	}else if actualuser.Password!=olduser.Password{
+		fmt.Println(*olduser," Password Incorrect !!!")
+		c.Data["json"]=map[string]interface{}{"code":6,"message":"密码错误"}
+		c.ServeJSON()
+		return
+	}else{
+		fmt.Println(*actualuser," User Login")
+		c.Data["json"]=map[string]interface{}{"code":0,"message":"登录成功"}
+		c.ServeJSON()
+		return
+	}
+}
+
